8254ea33b4e35edbdbd931cc21a7f9843fdcb755
Megan Jones <38929577+meginks@users.noreply.github.com>
2019-04-18 13:17:23 -0500

    fixed insert on hobbits model for sql

--- hobbits/hobbitsModel.js
+++ hobbits/hobbitsModel.js
@@ -9,11 +9,12 @@ module.exports = {
 };
 
 async function insert(hobbit) {
-  const [id] = await db('hobbits').insert(hobbit) 
+  const id = await db('hobbits').insert(hobbit, 'id') 
   return db('hobbits')
   .where({id}) 
   .first()
-}
+} 
+/// POSTGRES IS DIFFERENT FROM SQLITE -- postgres gives us a 0 or a 1 instead of an id like sqlite. you must add a second parameter into insert function so it will find the id on the object
 
 async function update(id, changes) {
   return null;
